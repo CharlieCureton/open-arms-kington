c03248c620efc6e104c31bfb1fd0cbc484029221
Charlie Cureton <ccureton21@gmail.com>
2021-12-02 16:36:54 +0000

    minor additions

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,13 +5,13 @@ function Home() {
     <div id="wrapper">
       <div className="home">
         <div className="container">
-          <div className="row align-items-center my-5">
-            Section 1
+          <div className="row align-items-center my-5" id="section_1">
+            <img src="https://d2j31icv6dlhz6.cloudfront.net/L/qPRAKAZ6WbaWEucS02Ic/Kington-GeneralView2.jpg"></img>
           </div>
-          <div className="row align-items-center my-5">
+          <div className="row align-items-center my-5" id="section_2">
             Section 2
           </div>
-          <div className="row align-items-center my-5">
+          <div className="row align-items-center my-5" id="section_3">
             Section 3
           </div>
         </div>
